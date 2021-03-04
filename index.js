0e2bb3d957a2fd5c0095ae3385b01dfe74ec04c3
patipon <61160103@go.buu.ac.th>
2021-03-04 19:40:42 +0700

    new

--- index.js
+++ index.js
@@ -1,62 +1,68 @@
 const express = require('express')
-const MongoClient = require('mongodb').MongoClient
-const ObjectId = require('mongodb').ObjectId
 const app = express()
 
 app.use(express.json())
+let books = [] //arry ตั้งแต่0->n-1 << before mongodb
 
 
+app.get('/books', (req, res) => {
+    //input*
 
-const uri = 'mongodb://superadmin:14511451@cluster0-shard-00-00.2xwft.mongodb.net:27017,cluster0-shard-00-01.2xwft.mongodb.net:27017,cluster0-shard-00-02.2xwft.mongodb.net:27017/buflix?ssl=true&replicaSet=atlas-50w25z-shard-0&authSource=admin&retryWrites=true&w=majority'
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true})
-let db, moviesCollection
+    //process*
 
-async function connect() {
-    await client.connect()
-     db = client.db('buflix')
-     moviesCollection = db.collection('movies')
-}
-connect()
-
-
-
-app.get('/movies', async (req, res) => {
- 
+    //output*
+    res.status(200).json(books)
 })
-app.get('/movies/:id', async (req, res) => {
+
+
+app.get('/books/:id', (req, res) =>{
+    //input*
     let id = req.params.id
-    const movie = await moviesCollection.findOne({ _id: ObjectId(id) })
-    res.status(200).json(movie)
+    // console.log(id: ${id}) <<เช็คว่า id ออกมาไหม
+     let book = {} //<<ประกาศเป็น object << before mongodb
+
+    //process*
+    // movie = movies[id] << before
+
+    //output*
+    res.status(200).json(book)
+
 })
-// 
-app.post('/movies', async (req, res) => {
-    // input
-    let newTitle = req.body.title
-    let newPlot  = req.body.plot
-    let newGenres = req.body.genres
-    let newCreators = req.body.creators
-    let newStars = req.body.stars
-    let newRating = req.body.rating
+//POST /movies
+//npm install --save-prod express <<ติดตั้ง express แบบ production
+//npm install --save-dev nodemon <<ติดตั้ง nodemon แบบ development dependency << restart server ให้ auto ไม่ต้องทำเอง
+//npm install --save-prod mongodb
+//npm init -y
+// เรียกใช้ nodemon index.js
+// << npm run dev ใช้ run dev ที่กำหนดใน package.json
 
+app.post('/books', (req, res) => { //<<แอโร่ function
 
-    let newMovie = {
-        title: newTitle,
-        plot: newPlot,
-        genres: newGenres,
-        creators: newCreators,
-        stars: newStars,
-        rating: newRating
+    //input*
+    let newtitle = req.body.title //<<ตรง.title กำหนดเองชื่ออื่นได้ 
+    let newprice = req.body.price 
+    let newunit = req.body.unit 
+    let newisbn = req.body.isbn 
+    let newimageurl = req.body.imageurl 
+
+    let newBook = {
+        title: newtitle, //key:value
+        price: newprice,
+        unit: newunit,
+        isbn: newisbn,
+        imageurl: newimageurl,
     }
-    let movieID = 0
-   
-    // process
-    const result = await moviesCollection.insertOne(newMovie)
-    //movies.push(newMovie)
-    // n-1
-   // movieID = movies.length - 1
-   movieID = result.insertedId
-    // output
-    res.status(201).json(movieID)
+    let bookID = 0
+
+    //process*
+
+   books.push(newBook) //<<insert ต่อท้ายไปเรื่อยๆ << before mongodb
+   bookID = books.length - 1 //<< before mongodb
+
+    //output*
+
+    res.status(201).json(bookID)
 })
+
 const port = 3000
 app.listen(port, () => console.log(`Server started again at ${port}`))
